07f64090fe353e1fab7ac4ae73ffff382771603f
payne-wu <wuzhipeng1289690157@gmail.com>
2022-10-24 17:42:59 +0800

    update it

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# hum
+hum
+===
+
+hum is fastapi deploy Best Practices
---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# coding: utf-8
+# Project:      hum
+# File:         __init__.py.py
+# Author:       payne-wu
+# Desc: 
+#
